1c1533a87630d28b39e291f79c250e720c09cc11
Ahmet Can Ak <ahmetcanak61@gmail.com>
2020-07-13 00:50:47 +0300

    zıplamayı geliştirdik, animasyonlar üzerinde çalışma yapılıyor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,57 +12,78 @@ public class PlayerController : MonoBehaviour
 
     public float JumpForce;
     public float Gravity;
+
+    public bool dead = false;
+    private string currentAnimation = null;
+
+    public Animation anim;
     
     void Start()
     {
         controller = GetComponent<CharacterController>();
         controller.enabled = true;
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.gameObject.name);
-        Debug.Log("versene");
+        anim = GetComponent<Animation>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction.z = forwardSpeed;
-        direction.y += Gravity * Time.deltaTime;
-
-        if (controller.isGrounded)
+        if (!dead)
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            direction.z = forwardSpeed;
+            direction.y += Gravity * Time.deltaTime;
+
+            if (controller.isGrounded)
             {
-                Jump();
+                if(currentAnimation != "Running")
+                {
+                    currentAnimation = "Running";
+                    anim.CrossFade(currentAnimation);
+
+                }
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+
+                    Jump();
+                }
             }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                desiredLane++;
+                if (desiredLane == 3)
+                    desiredLane = 2;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                desiredLane--;
+                if (desiredLane == -1)
+                    desiredLane = 0;
+            }
+
+            Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+            if (desiredLane == 0)
+            {
+                targetPosition += Vector3.left * laneDistance;
+            }
+            else if (desiredLane == 2)
+            {
+                targetPosition += Vector3.right * laneDistance;
+            }
+
+            transform.position = targetPosition;
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        else
         {
-            desiredLane++;
-            if (desiredLane == 3)
-                desiredLane = 2;
+            currentAnimation = "Dying";
+            anim.CrossFade(currentAnimation);
+            direction.z = 0;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            desiredLane--;
-            if (desiredLane == -1)
-                desiredLane = 0;
-        }
-
-        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
-        if(desiredLane == 0)
-        {
-            targetPosition += Vector3.left * laneDistance;
-        }
-        else if(desiredLane == 2)
-        {
-            targetPosition += Vector3.right * laneDistance;
-        }
-
-        transform.position = targetPosition;
+        
+        
     }
 
     private void FixedUpdate()
@@ -70,8 +91,16 @@ public class PlayerController : MonoBehaviour
         controller.Move(direction * Time.fixedDeltaTime);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("çarptı");
+        dead = true;
+    }
+
     private void Jump()
     {
+        currentAnimation = "Jump_3";
+        anim.CrossFade(currentAnimation);
         direction.y = JumpForce;
     }
 }
