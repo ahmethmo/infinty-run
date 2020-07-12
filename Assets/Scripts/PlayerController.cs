c1f70e2eef119b1770fc2a02a1d22641e995c6a2
ahmetcanak <ahmetcanak61@gmail.com>
2020-07-12 15:36:35 +0300

    Kamera takibi, yer şekillerinin otomatik olarak eklenmesi ve silinmesi, karakteri yon tuslari ile hareket ettirme

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller; 
+    private Vector3 direction;
+    public float forwardSpeed;
+    private int desiredLane = 1;
+    public float laneDistance;
+
+    public float JumpForce;
+    public float Gravity;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        controller.enabled = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        Debug.Log("versene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+        direction.y += Gravity * Time.deltaTime;
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Jump();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+                desiredLane = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if(desiredLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if(desiredLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        transform.position = targetPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+        direction.y = JumpForce;
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public Object[] yolPrefablari;
+    public float zSpawn = 0;
+    public float yolUzunlugu = 7.6f;
+    public List<GameObject> aktifYollar;
+    public Transform playerPos;
+
+    void Start()
+    {
+        yolPrefablari = Resources.LoadAll("Tiles", typeof(GameObject));
+            aktifYollar.Add(YolOlustur(0));
+            aktifYollar.Add(YolOlustur(0));
+            aktifYollar.Add(YolOlustur(0));
+            aktifYollar.Add(YolOlustur(0));
+            aktifYollar.Add(YolOlustur(0));
+            aktifYollar.Add(YolOlustur(0));
+            aktifYollar.Add(YolOlustur(0));
+        
+    }
+
+    private void Update()
+    {
+        if (aktifYollar.Count < 7)
+            aktifYollar.Add(YolOlustur(Random.Range(0, yolPrefablari.Length)));
+        if (playerPos.position.z -5 > zSpawn - (aktifYollar.Count * yolUzunlugu))
+            DeleteTile();
+    }
+
+    public GameObject YolOlustur(int yolIndex)
+    {
+        GameObject sa = (GameObject)Instantiate(yolPrefablari[yolIndex], transform.forward * zSpawn, transform.rotation); 
+        zSpawn += yolUzunlugu;
+        return sa;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(aktifYollar[0]);
+        aktifYollar.RemoveAt(0);
+    }
+
+
+
+
+}
